8bfb594fa295a9c6db4e2d7dd579d510ce8b5d33
AleXandra Rudivickaya <rag@one-touch.ru>
2021-03-24 20:40:23 +0300

    minify

--- wp-content/themes/medclinic/webpack.mix.js
+++ wp-content/themes/medclinic/webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
 
 mix.copy('node_modules/vue/dist/vue.js', 'dist/js')
-    .js('src/js/app.js', 'dist/js').minify('dist/js/app.js').sourceMaps()
-    .sass('src/css/app.sass', 'dist/css').options({processCssUrls: false}).minify('dist/css/app.css').sourceMaps();
+    .js('src/js/app.js', 'dist/js').sourceMaps().minify('dist/js/app.js')
+    .sass('src/css/app.sass', 'dist/css').options({processCssUrls: false}).sourceMaps().minify('dist/css/app.css');
