0a241ee368def39a1250fb7cccfb552c4a5a85ef
AleXandra Rudivickaya <rag@one-touch.ru>
2021-03-24 10:58:34 +0300

    add laravel-mix

--- wp-content/themes/medclinic/functions.php
+++ wp-content/themes/medclinic/functions.php
@@ -143,8 +143,6 @@ function medclinic_scripts() {
 	wp_enqueue_style( 'medclinic-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'medclinic-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'medclinic-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
---
+++ wp-content/themes/medclinic/webpack.mix.js
@@ -0,0 +1,6 @@
+let mix = require('laravel-mix');
+
+//mix.extract(['vue'])
+
+mix.js('src/js/app.js', 'dist/js')
+    .sass('src/css/app.sass', 'dist/css').options({processCssUrls: false});
