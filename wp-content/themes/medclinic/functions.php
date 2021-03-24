6c3144e54ec2de1252480085ce3e247f74ffd003
AleXandra Rudivickaya <rag@one-touch.ru>
2021-03-24 14:26:29 +0300

    scripts

--- wp-content/themes/medclinic/functions.php
+++ wp-content/themes/medclinic/functions.php
@@ -147,7 +147,7 @@ function medclinic_scripts() {
 
     wp_enqueue_script( 'medclinic-script', get_template_directory() . '/dist/js/manifest.js', array(), _S_VERSION  );
 
-    wp_enqueue_script( 'medclinic-script', get_template_directory() . '/dist/js/app.js', array(), _S_VERSION  );
+    wp_enqueue_script( 'medclinic-script-app', get_template_directory() . '/dist/js/app.js', array(), _S_VERSION  );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
