3907e4156d2df5b1b19dd87844add45dbf53b6ff
AleXandra Rudivickaya <rag@one-touch.ru>
2021-03-24 12:07:22 +0300

    add laravel-mix

---
+++ wp-content/themes/medclinic/dist/js/app.js
@@ -0,0 +1,34 @@
+(self["webpackChunkmedclinic"] = self["webpackChunkmedclinic"] || []).push([["/dist/js/app"],{
+
+/***/ "./src/js/app.js":
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
+/***/ (() => {
+
+console.log('Hello');
+
+/***/ }),
+
+/***/ "./src/css/app.sass":
+/*!**************************!*\
+  !*** ./src/css/app.sass ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["dist/css/app"], () => (__webpack_exec__("./src/js/app.js"), __webpack_exec__("./src/css/app.sass")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
--- wp-content/themes/medclinic/webpack.mix.js
+++ wp-content/themes/medclinic/webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 
-//mix.extract(['vue'])
+mix.extract(['vue']);
 
 mix.js('src/js/app.js', 'dist/js')
     .sass('src/css/app.sass', 'dist/css').options({processCssUrls: false});
